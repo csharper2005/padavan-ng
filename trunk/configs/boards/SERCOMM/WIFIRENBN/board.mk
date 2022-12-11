4b477e7ce0374f5751cd2a993f47b6b67540f8bb
Mikhail Zhilkin <csharper2005@gmail.com>
2022-12-11 15:41:36 +0000

    Add support for WiFire S1500.NBN
    This adds support for WiFire S1500.NBN
    
    Installation guide
    ------------------
    1. Install OpenWrt
    2. Make Factory mtd backup
    3. Install Breed bootloader for SmartBox Pro using "mtd write" command
       Link: https://github.com/legale/breed-mt7621-xiaomi-r3g.bin-reset-button-changer/tree/main/smartbox-flash-giga-turbo-pro
    4. Reboot ant enter the Breed web interface
    5. Trim Factory backup to 0x40000 (256 кБ) and restore it
    6. Edit MAC addresses:
          - LAN - from the backplate label
          - WLAN 2g - label + 1
          - WLAN 5g - label + 2
    7. Write firmware image to the second slot
    8. Set autoboot from 0x600000
    
    Signed-off-by: Mikhail Zhilkin <csharper2005@gmail.com>

---
+++ trunk/configs/boards/SERCOMM/WIFIRENBN/board.mk
@@ -0,0 +1,8 @@
+##################################################################
+# Board PID # Board Name       # PRODUCT # Note
+##################################################################
+# WIFIRENBN # WiFire S1500.NBN # MT7621  # csharper2005
+##################################################################
+
+CFLAGS += -DBOARD_WIFIRENBN -DVENDOR_SERCOMM
+BOARD_NUM_USB_PORTS=1
